af6b91050e3780d36f017f29b398d1e8d406b1b2
Louhan-dev <58785171+Louhan-dev@users.noreply.github.com>
2022-12-02 15:46:41 -0300

    Update installGlobalProtect.sh

--- installGlobalProtect.sh
+++ installGlobalProtect.sh
@@ -1,12 +1,14 @@
+[[ "$UID" -ne '0' ]] && { echo "Root is required."; exit 1 ;}
+
 #Install Dependencies
-sudo apt-get install libqt5webkit5
+apt-get install libqt5webkit5
 
 #Install GlobalProtect base
 wget -P /tmp "https://people.cs.ksu.edu/~sgsax/files/gpvpn/GlobalProtect_deb-5.1.1.0-17.deb"
-sudo yes | sudo dpkg -i /tmp/GlobalProtect_deb-5.1.1.0-17.deb
-sudo rm /tmp/GlobalProtect_deb-5.1.1.0-17.deb
+yes | dpkg -i /tmp/GlobalProtect_deb-5.1.1.0-17.deb
+rm /tmp/GlobalProtect_deb-5.1.1.0-17.deb
 
 #Install GlobalProtect UI
 wget -P /tmp "https://people.cs.ksu.edu/~sgsax/files/gpvpn/GlobalProtect_UI_deb-5.1.1.0-17.deb"
-sudo yes | sudo dpkg -i /tmp/GlobalProtect_UI_deb-5.1.1.0-17.deb
-sudo rm /tmp/GlobalProtect_UI_deb-5.1.1.0-17.deb
+yes | dpkg -i /tmp/GlobalProtect_UI_deb-5.1.1.0-17.deb
+rm /tmp/GlobalProtect_UI_deb-5.1.1.0-17.deb
